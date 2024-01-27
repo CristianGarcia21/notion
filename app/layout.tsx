081cfbcb56fa2ee142082b5c9f054f6857f7880b
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-01-27 13:55:48 -0500

    Configuration: Authentication with convex, initial configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,15 +35,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-          < ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="jotion-theme"
-          >
-            {children}
-          </ThemeProvider>
+          <ConvexClientProvider>
+            < ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="jotion-theme"
+            >
+              {children}
+            </ThemeProvider>
+          </ConvexClientProvider>
         </body>
     </html>
   );
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://polished-lamb-62.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
