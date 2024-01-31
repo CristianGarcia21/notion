7b83d78ed74ebefc8319adb9f97d626bb4171263
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-01-31 17:01:45 -0500

    Added: Settings  functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -18,9 +18,11 @@ import {
 } from "@/components/ui/popover";
 import { TrashBox } from "./trash-box"
 import { useSearch } from "@/hooks/use-search"
+import { useSettings } from "@/hooks/use-settings"
 
 
 export const Navigation = () => {
+    const settings = useSettings();
     const search = useSearch();
     const pathname = usePathname()
     const isMobile = useMediaQuery("(max-width: 768px")
@@ -142,7 +144,7 @@ export const Navigation = () => {
                     <UserItem />
 
                     <Item label="Search" icon={Search} isSearch onClick={search.onOpen}/>
-                    <Item label="Settings" icon={Settings}  onClick={() => {}}/>
+                    <Item label="Settings" icon={Settings}  onClick={settings.onOpen}/>
                     <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
                </div>
                <div className="mt-4">
