2f4fe955e192060bae32428ac2df66c91cdba428
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-02-03 10:41:55 -0500

    Fixed: page redirection and document id error deleted in path

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -49,6 +49,7 @@ export const Item = ({
         event.stopPropagation();
         if(!id) return;
         const promise = archive({ id})
+            .then(() => router.push("/documents"))
 
         toast.promise(promise, {
             loading: "Moving to trash...",
@@ -75,7 +76,7 @@ export const Item = ({
                 if (!expanded) {
                     onExpand?.();
                 }
-               // router.push(`/documents/${documentId}`)
+                router.push(`/documents/${documentId}`)
             });
 
             toast.promise(promise,{
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils"
 import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react"
-import { useParams, usePathname } from "next/navigation"
+import { useParams, usePathname, useRouter } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import { UserItem } from "./user-item"
@@ -24,6 +24,7 @@ import { Navbar } from "./navbar"
 
 
 export const Navigation = () => {
+    const router = useRouter();
     const settings = useSettings();
     const search = useSearch();
     const params = useParams();
@@ -115,7 +116,8 @@ export const Navigation = () => {
     }
 
     const handleCreate = () => {
-        const promise = create({ title: "Untitled" });
+        const promise = create({ title: "Untitled" })
+            .then((documentId) => router.push(`/documents/${documentId}`))
 
         toast.promise(promise, {
             loading: "Creating a new note...",
